401e2679757b975fc722ba26e781399ddf4e6f8a
Simon Dean <simon@simondean.org>
2021-09-27 00:11:31 +0100

    Add logging

--- service/src/main/java/tech/kronicle/service/repofinders/github/GitHubRepoFinder.java
+++ service/src/main/java/tech/kronicle/service/repofinders/github/GitHubRepoFinder.java
@@ -1,19 +1,23 @@
 package tech.kronicle.service.repofinders.github;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import tech.kronicle.service.models.ApiRepo;
 import tech.kronicle.service.repofinders.RepoFinder;
 import tech.kronicle.service.repofinders.github.client.GitHubClient;
 import tech.kronicle.service.repofinders.github.config.GitHubRepoFinderConfig;
+import tech.kronicle.service.repofinders.github.config.GitHubRepoFinderUserConfig;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import static java.util.Objects.isNull;
 
 @tech.kronicle.service.spring.stereotypes.RepoFinder
 @RequiredArgsConstructor
+@Slf4j
 public class GitHubRepoFinder extends RepoFinder {
 
   private final GitHubRepoFinderConfig config;
@@ -21,14 +25,18 @@ public class GitHubRepoFinder extends RepoFinder {
 
   @Override
   public List<ApiRepo> findApiRepos() {
-    if (isNull(config.getUsers())) {
-      return List.of();
-    }
+    List<GitHubRepoFinderUserConfig> users = getUsers();
+    log.info("Found {} GitHub users", users.size());
+    List<ApiRepo> apiRepos = users.stream()
+            .map(client::getRepos)
+            .flatMap(Collection::stream)
+            .distinct()
+            .collect(Collectors.toList());
+    log.info("Found {} API repos", apiRepos.size());
+    return apiRepos;
+  }
 
-    return config.getUsers().stream()
-      .map(client::getRepos)
-      .flatMap(Collection::stream)
-      .distinct()
-      .collect(Collectors.toList());
+  private List<GitHubRepoFinderUserConfig> getUsers() {
+    return Optional.ofNullable(config.getUsers()).orElseGet(List::of);
   }
 }
