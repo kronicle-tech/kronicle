3604c9708d2cf3a069f86ec4a0f3f0b5f9e0399e
Simon Dean <simon@simondean.org>
2021-10-03 16:47:47 +0100

    Remove wildcard Java import

--- service/src/main/java/tech/kronicle/service/scanners/sonarqube/client/SonarQubeClient.java
+++ service/src/main/java/tech/kronicle/service/scanners/sonarqube/client/SonarQubeClient.java
@@ -24,7 +24,10 @@ import org.springframework.web.reactive.function.client.WebClientResponseExcepti
 import reactor.core.publisher.Mono;
 
 import java.time.Duration;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
