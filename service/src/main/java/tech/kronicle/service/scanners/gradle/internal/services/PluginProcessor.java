9cca997de97f2c642772db5248a5fd7ebf9d54d1
Simon Dean <simon@simondean.org>
2021-10-03 23:30:25 +0100

    Fix bug with Gradle scan and none applied plugins (#68)

--- service/src/main/java/tech/kronicle/service/scanners/gradle/internal/services/PluginProcessor.java
+++ service/src/main/java/tech/kronicle/service/scanners/gradle/internal/services/PluginProcessor.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import tech.kronicle.service.scanners.gradle.internal.constants.GradlePlugins;
 
+import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
@@ -19,16 +20,34 @@ import static java.util.Objects.requireNonNull;
 @RequiredArgsConstructor
 public class PluginProcessor {
 
-    public void processPlugin(String scannerId, String name, String version, Set<Software> software) {
+    private static final List<SoftwareType> PLUGIN_SOFTWARE_TYPES = List.of(SoftwareType.GRADLE_PLUGIN, SoftwareType.GRADLE_PLUGIN_VERSION);
+
+    public void processPlugin(String scannerId, String name, String version, boolean apply, Set<Software> software) {
         requireNonNull(name, "name");
 
         software.add(Software.builder()
                 .scannerId(scannerId)
-                .type(SoftwareType.GRADLE_PLUGIN)
+                .type(apply ? SoftwareType.GRADLE_PLUGIN : SoftwareType.GRADLE_PLUGIN_VERSION)
                 .dependencyType(SoftwareDependencyType.DIRECT)
                 .name(name)
-                .version(version)
+                .version(getPluginVersion(name, version, software))
                 .build());
+    }
+
+    private String getPluginVersion(String name, String version, Set<Software> software) {
+        if (nonNull(version)) {
+            return version;
+        }
+
+        return getPlugin(name, software)
+                .map(Software::getVersion)
+                .orElse(null);
+    }
+
+    public Optional<Software> getPlugin(String name, Set<Software> software) {
+        return software.stream()
+                .filter(item -> nonNull(item.getType()) && PLUGIN_SOFTWARE_TYPES.contains(item.getType()) && Objects.equals(item.getName(), name))
+                .findFirst();
     }
 
     public int getPluginCount(Set<Software> software) {
@@ -37,7 +56,7 @@ public class PluginProcessor {
                 .count();
     }
 
-    public Optional<Software> getSpringBootPlugin(InheritingHashSet<Software> software) {
+    public Optional<Software> getSpringBootPlugin(Set<Software> software) {
         return software.stream()
                 .filter(item -> Objects.equals(item.getType(), SoftwareType.GRADLE_PLUGIN)
                         && Objects.equals(item.getName(), GradlePlugins.SPRING_BOOT)
