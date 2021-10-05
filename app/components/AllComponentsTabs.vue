d551cce39cbe89f63365dbb9b03531efea98605b
Simon Dean <simon@simondean.org>
2021-10-05 23:14:21 +0100

    Fix margin between tabs when wrapping (#79)

--- app/components/AllComponentsTabs.vue
+++ app/components/AllComponentsTabs.vue
@@ -1,26 +1,26 @@
 <template>
   <div class="my-3">
-    <b-button :to="{ name: 'all-components' }">Components</b-button>
+    <b-button class="my-1" :to="{ name: 'all-components' }">Components</b-button>
     <b-button
       :to="{ name: 'all-components-tests', query: { testOutcome: 'fail' } }"
     >
       Tests
     </b-button>
-    <b-button :to="{ name: 'all-components-cross-functional-requirements' }">
+    <b-button class="my-1" :to="{ name: 'all-components-cross-functional-requirements' }">
       Cross Functional Requirements
     </b-button>
-    <b-button :to="{ name: 'all-components-tech-debts' }">Tech Debts</b-button>
-    <b-button :to="{ name: 'all-components-dependencies' }">
+    <b-button class="my-1" :to="{ name: 'all-components-tech-debts' }">Tech Debts</b-button>
+    <b-button class="my-1" :to="{ name: 'all-components-dependencies' }">
       Dependencies
     </b-button>
-    <b-button :to="{ name: 'all-components-openapi-specs' }">
+    <b-button class="my-1" :to="{ name: 'all-components-openapi-specs' }">
       OpenAPI Specs
     </b-button>
-    <b-button :to="{ name: 'all-components-key-software' }">
+    <b-button class="my-1" :to="{ name: 'all-components-key-software' }">
       Key Software
     </b-button>
-    <b-button :to="{ name: 'all-components-repos' }">Repos</b-button>
-    <b-button :to="{ name: 'all-components-errors' }">Errors</b-button>
+    <b-button class="my-1" :to="{ name: 'all-components-repos' }">Repos</b-button>
+    <b-button class="my-1" :to="{ name: 'all-components-errors' }">Errors</b-button>
   </div>
 </template>
 
--- app/components/ComponentTabs.vue
+++ app/components/ComponentTabs.vue
@@ -1,40 +1,40 @@
 <template>
   <div class="my-3">
-    <b-button :to="`/components/${componentId}`">Info</b-button>
-    <b-button :to="`/components/${componentId}/dependencies`">
+    <b-button class="my-1" :to="`/components/${componentId}`">Info</b-button>
+    <b-button class="my-1" :to="`/components/${componentId}/dependencies`">
       Dependencies
     </b-button>
-    <b-button :to="`/components/${componentId}/response-times`">
+    <b-button class="my-1" :to="`/components/${componentId}/response-times`">
       Response Times
     </b-button>
-    <b-button :to="`/components/${componentId}/downstream-response-times`">
+    <b-button class="my-1" :to="`/components/${componentId}/downstream-response-times`">
       Downstream Response Times
     </b-button>
-    <b-button :to="`/components/${componentId}/call-graphs`">
+    <b-button class="my-1" :to="`/components/${componentId}/call-graphs`">
       Call Graphs
     </b-button>
-    <b-button :to="`/components/${componentId}/cross-functional-requirements`">
+    <b-button class="my-1" :to="`/components/${componentId}/cross-functional-requirements`">
       Cross Functional Requirements
     </b-button>
-    <b-button :to="`/components/${componentId}/tech-debts`">
+    <b-button class="my-1" :to="`/components/${componentId}/tech-debts`">
       Tech Debts
     </b-button>
-    <b-button :to="`/components/${componentId}/tests`">Tests</b-button>
-    <b-button :to="`/components/${componentId}/errors`">Errors</b-button>
-    <b-button :to="`/components/${componentId}/openapi-specs`">
+    <b-button class="my-1" :to="`/components/${componentId}/tests`">Tests</b-button>
+    <b-button class="my-1" :to="`/components/${componentId}/errors`">Errors</b-button>
+    <b-button class="my-1" :to="`/components/${componentId}/openapi-specs`">
       OpenAPI Specs
     </b-button>
-    <b-button :to="`/components/${componentId}/repo`">Repo</b-button>
-    <b-button :to="`/components/${componentId}/readme`">README</b-button>
-    <b-button :to="`/components/${componentId}/to-dos`">To Dos</b-button>
-    <b-button :to="`/components/${componentId}/lines-of-code`">
+    <b-button class="my-1" :to="`/components/${componentId}/repo`">Repo</b-button>
+    <b-button class="my-1" :to="`/components/${componentId}/readme`">README</b-button>
+    <b-button class="my-1" :to="`/components/${componentId}/to-dos`">To Dos</b-button>
+    <b-button class="my-1" :to="`/components/${componentId}/lines-of-code`">
       Lines of Code
     </b-button>
-    <b-button :to="`/components/${componentId}/software-repositories`">
+    <b-button class="my-1" :to="`/components/${componentId}/software-repositories`">
       Software Repositories
     </b-button>
-    <b-button :to="`/components/${componentId}/software`">Software</b-button>
-    <b-button :to="`/components/${componentId}/imports`">Imports</b-button>
+    <b-button class="my-1" :to="`/components/${componentId}/software`">Software</b-button>
+    <b-button class="my-1" :to="`/components/${componentId}/imports`">Imports</b-button>
   </div>
 </template>
 
