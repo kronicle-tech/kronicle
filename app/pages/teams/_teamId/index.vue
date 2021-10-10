79de60fc7f14951641295cd9882a453827084997
Simon Dean <simon@simondean.org>
2021-10-10 18:38:07 +0100

    Improve Team page (#100)

--- app/pages/teams/_teamId/index.vue
+++ app/pages/teams/_teamId/index.vue
@@ -3,36 +3,41 @@
     <h1 class="text-info my-3">{{ team.name }} Team</h1>
     <TeamTabs :team-id="team.id" />
 
-    <b-card v-if="team.areaId" title="Area" class="my-3">
-      <AreaName :area="{ id: team.areaId }" />
-    </b-card>
+    <b-card-group columns>
+      <b-card title="Team Name">
+        {{ team.name }}
+      </b-card>
 
-    <b-card
-      v-if="team.links && team.links.length > 0"
-      title="Links"
-      class="my-3"
-    >
-      <Links :links="team.links" />
-    </b-card>
+      <b-card v-if="team.areaId" title="Area">
+        <AreaName :area="{ id: team.areaId }" />
+      </b-card>
 
-    <b-card v-if="team.description" title="Description" class="my-3">
-      <Markdown :markdown="team.description" />
-    </b-card>
+      <b-card
+        v-if="team.links && team.links.length > 0"
+        title="Links"
+      >
+        <Links :links="team.links" />
+      </b-card>
 
-    <b-card v-if="team.emailAddress" title="Email Address" class="my-3">
-      <EmailAddress :email-address="team.emailAddress" />
-    </b-card>
+      <b-card v-if="team.description" title="Description">
+        <Markdown :markdown="team.description" />
+      </b-card>
 
-    <b-card v-if="team.notes" title="Notes" class="my-3">
-      <Markdown :markdown="team.notes" :toc="true" />
-    </b-card>
+      <b-card v-if="team.notes" title="Notes">
+        <Markdown :markdown="team.notes" :toc="true" />
+      </b-card>
+
+      <b-card v-if="team.emailAddress" title="Email Address">
+        <EmailAddress :email-address="team.emailAddress" />
+      </b-card>
+    </b-card-group>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import { MetaInfo } from 'vue-meta'
-import { BCard } from 'bootstrap-vue'
+import { BCard, BCardGroup } from 'bootstrap-vue'
 import { Team } from '~/types/kronicle-service'
 import AreaName from '~/components/AreaName.vue'
 import EmailAddress from '~/components/EmailAddress.vue'
@@ -44,6 +49,7 @@ export default Vue.extend({
   components: {
     AreaName,
     'b-card': BCard,
+    'b-card-group': BCardGroup,
     EmailAddress,
     Links,
     Markdown,
