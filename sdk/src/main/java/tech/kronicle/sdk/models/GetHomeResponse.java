91fcbcef686d0e99f908885c5c45a68998756326
Simon Dean <simon@simondean.org>
2022-02-04 00:29:45 +0000

    Add Home and Health resources (#217)

--- sdk/src/main/java/tech/kronicle/sdk/models/GetHomeResponse.java
+++
@@ -1,8 +0,0 @@
-package tech.kronicle.sdk.models;
-
-import lombok.Value;
-
-@Value
-public class GetHomeResponse {
-
-}
--- service/src/main/java/tech/kronicle/service/controllers/HealthController.java
+++ service/src/main/java/tech/kronicle/service/controllers/HealthController.java
@@ -5,8 +5,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import tech.kronicle.sdk.models.GetHealthResponse;
-import tech.kronicle.sdk.models.GetHomeResponse;
 import tech.kronicle.service.partialresponse.PartialResponse;
 
 @RestController
@@ -17,12 +15,12 @@ public class HealthController {
     @Operation(
             tags = {"Home"},
             summary = "Get Health",
-            description = "Returns a 200 status code.  ",
+            description = "Returns the word OK.  ",
             operationId = "get-health"
     )
     @GetMapping
     @PartialResponse
-    public GetHealthResponse getHealth() {
-        return new GetHealthResponse();
+    public String getHealth() {
+        return "OK";
     }
 }
