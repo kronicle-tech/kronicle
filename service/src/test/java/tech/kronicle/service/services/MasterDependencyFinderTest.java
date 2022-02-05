543388875a624aaab07e7af739ed786b965e56d6
Simon Dean <simon@simondean.org>
2022-02-05 09:40:27 +0000

    Dependency finders - add logging (#222)

--- service/src/test/java/tech/kronicle/service/services/MasterDependencyFinderTest.java
+++ service/src/test/java/tech/kronicle/service/services/MasterDependencyFinderTest.java
@@ -1,5 +1,8 @@
 package tech.kronicle.service.services;
 
+import ch.qos.logback.classic.Level;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -7,6 +10,9 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import tech.kronicle.componentmetadata.models.ComponentMetadata;
 import tech.kronicle.sdk.models.Dependency;
 import tech.kronicle.service.finders.DependencyFinder;
+import tech.kronicle.service.scanners.gradle.internal.services.BillOfMaterialsLogger;
+import tech.kronicle.service.testutils.LogCaptor;
+import tech.kronicle.service.testutils.SimplifiedLogEvent;
 
 import java.util.List;
 
@@ -23,6 +29,17 @@ public class MasterDependencyFinderTest {
     private DependencyFinder dependencyFinder1;
     @Mock
     private DependencyFinder dependencyFinder2;
+    private LogCaptor logCaptor;
+
+    @BeforeEach
+    public void beforeEach() {
+        logCaptor = new LogCaptor(MasterDependencyFinder.class);
+    }
+
+    @AfterEach
+    public void afterEach() {
+        logCaptor.close();
+    }
 
     @Test
     public void getDependenciesShouldReturnAllDependenciesFromAllDependencyFinders() {
@@ -61,5 +78,28 @@ public class MasterDependencyFinderTest {
 
         // Then
         assertThat(returnValue).containsExactly(dependency1, dependency2, dependency3);
+    }
+
+    @Test
+    public void getDependenciesShouldLogAndIgnoreAnExceptionWhenExecutingDependencyFinders() {
+        // Given
+        underTest = new MasterDependencyFinder(finderRegistry);
+        when(finderRegistry.getDependencyFinders()).thenReturn(List.of(dependencyFinder1, dependencyFinder2));
+        ComponentMetadata componentMetadata = ComponentMetadata.builder().build();
+        when(dependencyFinder1.id()).thenReturn("test-dependency-finder-1");
+        when(dependencyFinder1.find(componentMetadata)).thenThrow(new RuntimeException("Fake exception"));
+        Dependency dependency1 = new Dependency("test-service-1", "test-service-2");
+        Dependency dependency2 = new Dependency("test-service-3", "test-service-4");
+        when(dependencyFinder2.id()).thenReturn("test-dependency-finder-2");
+        when(dependencyFinder2.find(componentMetadata)).thenReturn(List.of(dependency1, dependency2));
+
+        // When
+        List<Dependency> returnValue = underTest.getDependencies(componentMetadata);
+
+        // Then
+        assertThat(returnValue).containsExactly(dependency1, dependency2);
+        assertThat(logCaptor.getSimplifiedEvents()).containsExactly(
+                new SimplifiedLogEvent(Level.ERROR, "Failed to execute dependency finder test-dependency-finder-1"),
+                new SimplifiedLogEvent(Level.INFO, "Dependency finder test-dependency-finder-2 found 2 dependencies"));
     }
 }
