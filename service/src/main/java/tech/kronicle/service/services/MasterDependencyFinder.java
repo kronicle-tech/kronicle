0f941c2425db20aef0f8d276c0bb11ba94f15289
Simon Dean <simon@simondean.org>
2022-02-05 09:24:55 +0000

    Dependency finders - add logging

--- service/src/main/java/tech/kronicle/service/services/MasterDependencyFinder.java
+++ service/src/main/java/tech/kronicle/service/services/MasterDependencyFinder.java
@@ -1,15 +1,18 @@
 package tech.kronicle.service.services;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import tech.kronicle.componentmetadata.models.ComponentMetadata;
 import tech.kronicle.sdk.models.Dependency;
+import tech.kronicle.service.finders.DependencyFinder;
 
 import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class MasterDependencyFinder {
 
@@ -17,9 +20,22 @@ public class MasterDependencyFinder {
 
     public List<Dependency> getDependencies(ComponentMetadata componentMetadata) {
         return finderRegistry.getDependencyFinders().stream()
-                .map(finder -> finder.find(componentMetadata))
+                .map(finder -> executeFinder(finder, componentMetadata))
                 .flatMap(Collection::stream)
                 .distinct()
                 .collect(Collectors.toList());
+    }
+
+    private List<Dependency> executeFinder(DependencyFinder finder, ComponentMetadata componentMetadata) {
+        List<Dependency> dependencies;
+        try {
+            dependencies = finder.find(componentMetadata);
+        } catch (Exception e) {
+            log.error("Failed to execute dependency finder {}", finder.id(), e);
+            return List.of();
+        }
+
+        log.info("Dependency finder {} found {} dependencies", finder.id(), dependencies.size());
+        return dependencies;
     }
 }
