b2edbd73c07bfaddbd384ec4389a66e6de238ae8
Simon Dean <simon@simondean.org>
2022-02-10 19:19:48 +0000

    Use distroless (#236)

---
+++ app/bin/healthcheck.js
@@ -0,0 +1,20 @@
+const http = require("http");
+const options = {
+  host: process.env.HOST,
+  port: process.env.PORT,
+  timeout: process.env.HEALTHCHECK_TIMEOUT || 10_000,
+};
+
+const request = http.request(options, res => {
+  console.log(`Response status code: ${res.statusCode}`)
+  if (res.statusCode === 200) {
+    process.exit(0);
+  } else {
+    process.exit(1);
+  }
+});
+
+request.on("error", err => {
+  console.error(`Error ${("message" in err) ? err.message : "{no err.message}"}`);
+  process.exit(1);
+});
