8bf29e95959a1aa14f5fda27f35f32c147b99a86
Simon Dean <simon@simondean.org>
2022-02-12 09:40:50 +0000

    Fix nodejs healthcheck

--- app/bin/healthcheck.js
+++ app/bin/healthcheck.js
@@ -1,11 +1,14 @@
 const http = require("http");
 const options = {
-  host: process.env.HOST || "0.0.0.0",
-  port: process.env.PORT || 3000,
-  timeout: process.env.HEALTHCHECK_TIMEOUT || 10_000,
+  host: "localhost",
+  port: 3000,
+  path: "/health",
+  timeout: 10_000,
 };
 
-const request = http.request(options, res => {
+console.log(`Options: ${JSON.stringify(options)}`)
+
+const req = http.request(options, res => {
   console.log(`Response status code: ${res.statusCode}`)
   if (res.statusCode === 200) {
     process.exit(0);
@@ -14,7 +17,9 @@ const request = http.request(options, res => {
   }
 });
 
-request.on("error", err => {
+req.on("error", err => {
   console.error(`Error: ${("message" in err) ? err.message : "{no err.message}"}`);
   process.exit(1);
 });
+
+req.end();
