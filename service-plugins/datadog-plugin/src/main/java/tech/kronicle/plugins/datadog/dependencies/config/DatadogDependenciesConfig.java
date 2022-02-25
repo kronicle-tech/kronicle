a7365fd4c7ec5fa30e37a70a619e887bdf9c4805
Simon Dean <simon@simondean.org>
2022-02-25 21:19:45 +0000

    Speed up builds

--- service-plugins/datadog-plugin/src/main/java/tech/kronicle/plugins/datadog/dependencies/config/DatadogDependenciesConfig.java
+++ service-plugins/datadog-plugin/src/main/java/tech/kronicle/plugins/datadog/dependencies/config/DatadogDependenciesConfig.java
@@ -2,7 +2,6 @@ package tech.kronicle.plugins.datadog.dependencies.config;
 
 import lombok.Value;
 import lombok.experimental.NonFinal;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.validation.annotation.Validated;
 
 import javax.validation.constraints.NotNull;
