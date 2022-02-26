83dd60e60e7451ed208f1b3be293c031a1b6c496
Simon Dean <simon@simondean.org>
2022-02-26 23:37:53 +0000

    Work in progress 2

--- service-plugins/github-plugin/src/main/java/tech/kronicle/plugins/github/config/GitHubOrganizationConfig.java
+++ service-plugins/github-plugin/src/main/java/tech/kronicle/plugins/github/config/GitHubOrganizationConfig.java
@@ -4,11 +4,14 @@ import lombok.Value;
 import lombok.experimental.NonFinal;
 import org.springframework.validation.annotation.Validated;
 
+import javax.validation.constraints.NotEmpty;
+
 @Validated
 @Value
 @NonFinal
 public class GitHubOrganizationConfig {
 
+    @NotEmpty
     String accountName;
     GitHubAccessTokenConfig accessToken;
 }
--- service-plugins/github-plugin/src/main/java/tech/kronicle/plugins/github/config/GitHubUserConfig.java
+++ service-plugins/github-plugin/src/main/java/tech/kronicle/plugins/github/config/GitHubUserConfig.java
@@ -4,11 +4,14 @@ import lombok.Value;
 import lombok.experimental.NonFinal;
 import org.springframework.validation.annotation.Validated;
 
+import javax.validation.constraints.NotEmpty;
+
 @Validated
 @Value
 @NonFinal
 public class GitHubUserConfig {
 
+    @NotEmpty
     String accountName;
     GitHubAccessTokenConfig accessToken;
 }
