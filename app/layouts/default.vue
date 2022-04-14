a3102795f166025053bfc6a5eb07328789abff8a
Simon Dean <simon@simondean.org>
2022-04-14 08:30:46 +0100

    Build state (#313)

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,13 +1,14 @@
 <template>
   <div>
     <b-navbar toggleable="sm" class="navbar navbar-dark bg-dark">
-      <b-navbar-brand to="/" class="text-primary"><b>Kronicle</b></b-navbar-brand>
+      <b-navbar-brand to="/" class="text-info"><b>Kronicle</b></b-navbar-brand>
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav>
           <b-nav-item to="/all-environments">Environments</b-nav-item>
           <b-nav-item to="/all-components">Components</b-nav-item>
           <b-nav-item to="/all-components/dependencies">Dependencies</b-nav-item>
+          <b-nav-item to="/all-components/openapi-specs">OpenAPI</b-nav-item>
           <b-nav-item to="/all-teams">Teams</b-nav-item>
           <b-nav-item to="/all-areas">Areas</b-nav-item>
           <b-nav-item to="/all-tests">Tests</b-nav-item>
