e11c49011eb6653a3fb95685c1f1860813877128
Simon Dean <simon@simondean.org>
2022-04-21 21:48:44 +0100

    Environment status improvements (#344)

--- service-plugins/openapi-plugin/src/test/java/tech/kronicle/plugins/openapi/guice/GuiceModuleTest.java
+++ service-plugins/openapi-plugin/src/test/java/tech/kronicle/plugins/openapi/guice/GuiceModuleTest.java
@@ -1,9 +1,11 @@
 package tech.kronicle.plugins.openapi.guice;
 
+import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import org.junit.jupiter.api.Test;
 import tech.kronicle.plugins.openapi.OpenApiScanner;
+import tech.kronicle.plugins.openapi.config.OpenApiConfig;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -14,12 +16,25 @@ public class GuiceModuleTest {
     @Test
     public void shouldCreateAOpenApiScannerInstance() {
         // Given
-        Injector guiceInjector = Guice.createInjector(underTest);
+        Injector guiceInjector = createGuiceInjector();
 
         // When
         OpenApiScanner returnValue = guiceInjector.getInstance(OpenApiScanner.class);
 
         // Then
         assertThat(returnValue).isNotNull();
+    }
+
+    private Injector createGuiceInjector() {
+        AbstractModule configModule = new AbstractModule() {
+            @Override
+            protected void configure() {
+                bind(OpenApiConfig.class).toInstance(new OpenApiConfig(
+                        null
+                ));
+            }
+        };
+        Injector guiceInjector = Guice.createInjector(underTest, configModule);
+        return guiceInjector;
     }
 }
