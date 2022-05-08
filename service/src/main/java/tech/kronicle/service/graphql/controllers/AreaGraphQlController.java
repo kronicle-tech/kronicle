cfe9ef92dcf3b3aec6f6f2556e1068d2d00d4511
Simon Dean <simon@simondean.org>
2022-05-08 21:30:02 +0100

    GraphQL (#374)

---
+++ service/src/main/java/tech/kronicle/service/graphql/controllers/AreaGraphQlController.java
@@ -0,0 +1,23 @@
+package tech.kronicle.service.graphql.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+import tech.kronicle.service.graphql.models.AreasOutput;
+import tech.kronicle.service.services.ComponentService;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class AreaGraphQlController {
+
+    private final ComponentService componentService;
+
+    @QueryMapping
+    public AreasOutput areas() {
+        return new AreasOutput(
+                componentService.getAreas(List.of())
+        );
+    }
+}
---
+++ service/src/main/java/tech/kronicle/service/graphql/controllers/TeamGraphQlController.java
@@ -0,0 +1,23 @@
+package tech.kronicle.service.graphql.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+import tech.kronicle.service.graphql.models.TeamsOutput;
+import tech.kronicle.service.services.ComponentService;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class TeamGraphQlController {
+
+    private final ComponentService componentService;
+
+    @QueryMapping
+    public TeamsOutput teams() {
+        return new TeamsOutput(
+                componentService.getTeams(List.of())
+        );
+    }
+}
