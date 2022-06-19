79f0a76099151eed0735cd3f19f05f080bbbc8b7
Simon Dean <simon@simondean.org>
2022-06-19 23:40:10 +0100

    Diagrams feature (#435)

---
+++ app/pages/teams/_teamId/graphql-schemas/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="m-3">
+    <h1 class="text-info my-3">{{ team.name }} - GraphQL Schemas</h1>
+
+    <TeamTabs :team-id="team.id" />
+
+    <GraphQlSchemasView :components="team.components" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { MetaInfo } from 'vue-meta'
+import { Team } from '~/types/kronicle-service'
+import TeamTabs from '~/components/TeamTabs.vue'
+import GraphQlSchemasView from '~/components/GraphQlSchemasView.vue'
+
+export default Vue.extend({
+  components: {
+    GraphQlSchemasView,
+    TeamTabs,
+  },
+  async asyncData({ $config, route, store }) {
+    const team = await fetch(
+      `${$config.serviceBaseUrl}/v1/teams/${route.params.teamId}?stateType=graphql-schemas&fields=team(id,name,components(id,name,typeId,tags,teams,platformId,states))`
+    )
+      .then((res) => res.json())
+      .then((json) => json.team as Team)
+
+    store.commit('componentFilters/initialize', {
+      components: team.components,
+      route,
+    })
+
+    return {
+      team,
+    }
+  },
+  data() {
+    return {
+      team: {} as Team,
+    }
+  },
+  head(): MetaInfo {
+    return {
+      title: `Kronicle - ${this.team.name} - GraphQL Schemas`,
+    }
+  },
+})
+</script>
--- app/test/pages/teams/_teamId/graphql-schemas/index.spec.js
+++ app/test/pages/teams/_teamId/graphql-schemas/index.spec.js
@@ -1,26 +1,26 @@
-import Index from '@/pages/areas/_areaId/openapi-specs/index.vue'
+import Index from '@/pages/teams/_teamId/graphql-schemas/index.vue'
 import { createPageWrapper } from '~/test/pages/pageUtils'
 import {
-  createArea,
+  createTeam,
   createComponent,
-  createComponentWithOpenApiSpecs,
+  createComponentWithGraphQlSchemas,
 } from '~/test/testDataUtils'
 
 describe('Index', () => {
   const route = {
     params: {
-      areaId: 'test-area-id-1',
+      teamId: 'test-team-id-1',
     },
   }
-  let area
+  let team
   let wrapper
   async function createWrapper() {
     wrapper = await createPageWrapper(Index, {
       route,
       serviceRequests: {
-        '/v1/areas/test-area-id-1?stateType=openapi-specs&fields=area(id,name,components(id,name,typeId,tags,teams,platformId,states))':
+        '/v1/teams/test-team-id-1?stateType=graphql-schemas&fields=team(id,name,components(id,name,typeId,tags,teams,platformId,states))':
           {
-            responseBody: { area },
+            responseBody: { team },
           },
       },
     })
@@ -31,50 +31,50 @@ describe('Index', () => {
     wrapper = null
   })
 
-  describe('when Get Area service endpoint returns an area', () => {
+  describe('when Get Team service endpoint returns an team', () => {
     beforeEach(() => {
-      area = createArea({ areaNumber: 1 })
+      team = createTeam({ teamNumber: 1 })
     })
 
     test('has the right page title', async () => {
       await createWrapper()
       expect(wrapper.vm.$metaInfo.title).toBe(
-        'Kronicle - Test Area Name 1 - OpenAPI Specs'
+        'Kronicle - Test Team Name 1 - GraphQL Schemas'
       )
     })
 
-    describe('when the area has no components', () => {
-      test('renders no OpenAPI specs', async () => {
+    describe('when the team has no components', () => {
+      test('renders no GraphQL Schemas', async () => {
         await createWrapper()
         expect(wrapper.element).toMatchSnapshot()
       })
     })
 
-    describe('when the area has components but the components have no OpenAPI specs', () => {
+    describe('when the team has components but the components have no GraphQL Schemas', () => {
       beforeEach(() => {
-        area.components = [
+        team.components = [
           createComponent({ componentNumber: 1 }),
           createComponent({ componentNumber: 2 }),
           createComponent({ componentNumber: 3 }),
         ]
       })
 
-      test('renders no OpenAPI specs', async () => {
+      test('renders no GraphQL Schemas', async () => {
         await createWrapper()
         expect(wrapper.element).toMatchSnapshot()
       })
     })
 
-    describe('when the area has components and the components have OpenAPI specs', () => {
+    describe('when the team has components and the components have GraphQL Schemas', () => {
       beforeEach(() => {
-        area.components = [
-          createComponentWithOpenApiSpecs({ componentNumber: 1 }),
+        team.components = [
+          createComponentWithGraphQlSchemas({ componentNumber: 1 }),
           createComponent({ componentNumber: 2 }),
-          createComponentWithOpenApiSpecs({ componentNumber: 3 }),
+          createComponentWithGraphQlSchemas({ componentNumber: 3 }),
         ]
       })
 
-      test('renders the details of the OpenAPI specs', async () => {
+      test('renders the details of the GraphQL Schemas', async () => {
         await createWrapper()
         expect(wrapper.element).toMatchSnapshot()
       })
