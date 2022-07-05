bfc169a15dfe84e086b228bc89b77941660aa948
Simon Dean <simon@simondean.org>
2022-07-05 08:38:13 +0100

    Key software as tags (#466)

--- app/components/Tags.vue
+++ app/components/Tags.vue
@@ -1,16 +1,25 @@
 <template>
-  <ul v-if="tags && tags.length > 0">
-    <li v-for="(tag, tagIndex) in tags" :key="tagIndex">
+  <div v-if="tags && tags.length > 0" class="h5">
+    <b-badge
+      v-for="(tag, tagIndex) in tags"
+      :key="tagIndex"
+      variant="info"
+      class="mr-2 mb-2"
+    >
       {{ tag.key }}<span v-if="tag.value !== undefined">: {{ tag.value }}</span>
-    </li>
-  </ul>
+    </b-badge>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue, { PropType } from 'vue'
+import { BBadge } from 'bootstrap-vue'
 import { Tag } from '~/types/kronicle-service'
 
 export default Vue.extend({
+  components: {
+    'b-badge': BBadge,
+  },
   props: {
     tags: {
       type: Array as PropType<Tag[]>,
