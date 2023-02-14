fe01c93ba6cf6f7aa128ec6f6bcad0bc5b47d3f0
Simon Dean <simon@simondean.org>
2023-02-14 21:55:03 +0000

    Improve E2E tests

--- app/cypress/e2e/components.cy.ts
+++ app/cypress/e2e/components.cy.ts
@@ -2,6 +2,7 @@ describe('components spec', () => {
   it('can load components from the backend service', () => {
     cy.visit('http://localhost:3000/')
     cy.get('[data-cy="nav-link-components"]').click()
+    cy.url().should('eq', 'http://localhost:3000/components')
     cy.get('a[href="/components/kronicle-app"]').contains('Kronicle App')
   })
 })
