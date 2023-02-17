f2a16683ba1f9b9c0f07f7b129280e9dd018a528
Simon Dean <simon@simondean.org>
2023-02-17 12:53:34 +0000

    Fix Service health check (#597)

--- service-health-check/src/main/java/HealthCheck.java
+++ service-health-check/src/main/java/HealthCheck.java
@@ -7,26 +7,27 @@ import static java.util.Optional.ofNullable;
 
 public class HealthCheck {
     public static void main(String[] args) {
-        var host = ofNullable(System.getenv("HOST"))
-                .orElse("0.0.0.0");
-        var port = ofNullable(System.getenv("PORT"))
-                .map(Integer::parseInt).orElse(8090);
-        var client = HttpClient.newHttpClient();
-        var request = HttpRequest.newBuilder()
-                .uri(URI.create("http://" + host + ":" + port + "/health"))
-                .header("accept", "*/*")
-                .build();
-        HttpResponse<String> response;
         try {
-            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            var host = ofNullable(System.getenv("HOST"))
+                    .orElse("0.0.0.0");
+            var port = ofNullable(System.getenv("PORT"))
+                    .map(Integer::parseInt).orElse(8090);
+            var client = HttpClient.newHttpClient();
+            var request = HttpRequest.newBuilder()
+                    .uri(URI.create("http://" + host + ":" + port + "/health"))
+                    .header("accept", "*/*")
+                    .build();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            if (response.statusCode() != 200) {
+                System.err.println("Health check FAIL with status code: " + response.statusCode());
+                System.exit(1);
+            } else {
+                System.err.println("Health check SUCCESS with status code: " + response.statusCode());
+            }
         } catch (Exception e) {
-            System.err.println("Health check failed with exception: " + e);
+            System.err.println("Health check FAIL with exception: " + e);
             System.exit(1);
             return;
-        }
-        if (response.statusCode() != 200) {
-            System.err.println("Health check failed with status code: " + response.statusCode());
-            System.exit(1);
         }
     }
 }
